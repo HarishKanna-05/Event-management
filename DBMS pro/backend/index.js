86bae394b1012bca7bb9265b8c3f02d98606907e
öpenwork1 <sekarsekar5017@gmail.com>
2024-05-20 20:06:21 +0530

    add:router

--- DBMS pro/backend/index.js
+++ DBMS pro/backend/index.js
@@ -1,52 +1,19 @@
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
-import { Event } from "./models/EventSchema.js";
+import eventRoutes from "./routes/eventRoutes.js";
 dotenv.config();
 const app = express();
 const PORT = 5000;
 app.use(express.json());
-app.post("/createEvent", async (req, res) => {
-  try {
-    if (
-      !req.body.eventName ||
-      !req.body.conductBy ||
-      !req.body.eventDate ||
-      !req.body.eventTime ||
-      !req.body.venue ||
-      !req.body.eventLink
-    ) {
-      return res.status(400).json({ message: "All fields are required" });
-    }
-    const { eventName, conductBy, eventDate, eventTime, venue, eventLink } =
-      req.body;
-    const event = new Event({
-      eventName,
-      conductBy,
-      eventDate: new Date(eventDate),
-      eventTime: new Date(eventTime),
-      venue,
-      eventLink,
-    });
-    await event.save();
-    return res.status(200).send({ message: "Event Created Sucesssfully" });
-  } catch (err) {
-    console.log(err);
-    res.status(500).send("An error occured while saving the event");
-  }
-});
-app.get("/getEvents", async (req, res) => {
-  try {
-    const events = await Event.find({});
-    res.status(200).json({ counts: events.length, data: events });
-  } catch (err) {
-    console.log(err);
-    res.status(500).send("An error occured while fetching the events");
-  }
-});
+app.use("/event", eventRoutes);
+
 app.listen(PORT, async () => {
   await mongoose
-    .connect("mongodb+srv://dhaya123:Dhaya%40123@cluster0.amircbx.mongodb.net/")
+    .connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     .then(() => {
       console.log(`Server is running on port ${PORT}`);
       console.log("connected to database!");
--- DBMS pro/backend/routes/eventRoutes.js
+++ DBMS pro/backend/routes/eventRoutes.js
@@ -1,4 +1,42 @@
 import express from "express";
 import { Event } from "../models/EventSchema.js";
-const router = express.Router();
-router.post;
+const route = express.Router();
+route.post("/", async (req, res) => {
+  try {
+    if (
+      !req.body.eventName ||
+      !req.body.conductBy ||
+      !req.body.eventDate ||
+      !req.body.eventTime ||
+      !req.body.venue ||
+      !req.body.eventLink
+    ) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    const { eventName, conductBy, eventDate, eventTime, venue, eventLink } =
+      req.body;
+    const event = new Event({
+      eventName,
+      conductBy,
+      eventDate: new Date(eventDate),
+      eventTime: new Date(eventTime),
+      venue,
+      eventLink,
+    });
+    await event.save();
+    return res.status(200).send({ message: "Event Created Sucesssfully" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("An error occured while saving the event");
+  }
+});
+route.get("/", async (req, res) => {
+  try {
+    const events = await Event.find({});
+    res.status(200).json({ counts: events.length, data: events });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("An error occured while fetching the events");
+  }
+});
+export default route;
